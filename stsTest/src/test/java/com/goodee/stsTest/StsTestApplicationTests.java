1f48579dd005a54dda4b3cb1306436a15af084e2
electricPo <electricPotatoGarden@gmail.com>
2023-07-24 09:34:56 +0900

    finalsts 초기화

---
+++ stsTest/src/test/java/com/goodee/stsTest/StsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.stsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
